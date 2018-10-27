03eee14b9f81901993b7cd0e4d84d6604d51a3c5
Valentin Fries <contact@fries.io>
2018-10-27 17:39:06 +0200

    US 3: Account initialization refactoring

--- src/main/java/fr/lacombe/account/Account.java
+++ src/main/java/fr/lacombe/account/Account.java
@@ -2,30 +2,21 @@ package fr.lacombe.account;
 
 import static fr.lacombe.account.Operation.DEPOSIT;
 import static fr.lacombe.account.Operation.WITHDRAWAL;
-import static java.util.Collections.singletonList;
 
 class Account {
 
-    private final Amount balance;
     private History history;
 
-    private Account(final Amount balance, final History history) {
-        this.balance = balance;
+    private Account(final History history) {
         this.history = history;
     }
 
     static Account empty() {
-        return new Account(Amount.of(0L), History.empty());
+        return new Account(History.empty());
     }
 
     static Account of(final History history) {
-        return new Account(Amount.of(0L), history);
-    }
-
-    static Account of(final Amount balance) {
-        return Account.of(History.of(singletonList(
-                OperationStatement.of(DEPOSIT, balance, balance)
-        )));
+        return new Account(history);
     }
 
     OperationStatement deposit(final Amount amount) {
--- src/test/java/fr/lacombe/account/AccountTest.java
+++ src/test/java/fr/lacombe/account/AccountTest.java
@@ -5,41 +5,42 @@ import org.junit.Test;
 import static fr.lacombe.account.Operation.DEPOSIT;
 import static fr.lacombe.account.Operation.WITHDRAWAL;
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
     public void should_add_the_deposit_amount_to_the_account_balance() {
-        final Account account = Account.of(Amount.of(4L));
-        final Amount depositAmount = Amount.of(2L);
+        final Account account = Account.of(History.of(singletonList(
+                OperationStatement.of(DEPOSIT, Amount.of(4L), Amount.of(4L))
+        )));
 
-        final OperationStatement statement = account.deposit(depositAmount);
+        final OperationStatement statement = account.deposit(Amount.of(2L));
 
-        assertThat(statement).isEqualTo(OperationStatement.of(
-                DEPOSIT,
-                Amount.of(2L),
-                Amount.of(6L)
-        ));
+        assertThat(statement).isEqualTo(
+                OperationStatement.of(DEPOSIT, Amount.of(2L), Amount.of(6L))
+        );
     }
 
     @Test
     public void should_subtract_the_withdrawal_amount_from_the_account_balance() {
-        final Account account = Account.of(Amount.of(4L));
-        final Amount withdrawalAmount = Amount.of(2L);
+        final Account account = Account.of(History.of(singletonList(
+                OperationStatement.of(DEPOSIT, Amount.of(4L), Amount.of(4L))
+        )));
 
-        final OperationStatement statement = account.withdraw(withdrawalAmount);
+        final OperationStatement statement = account.withdraw(Amount.of(2L));
 
-        assertThat(statement).isEqualTo(OperationStatement.of(
-                WITHDRAWAL,
-                Amount.of(2L),
-                Amount.of(2L)
-        ));
+        assertThat(statement).isEqualTo(
+                OperationStatement.of(WITHDRAWAL, Amount.of(2L), Amount.of(2L))
+        );
     }
 
     @Test
     public void should_have_a_detailed_operation_statement_in_the_account_history_when_i_make_a_deposit() {
-        final Account account = Account.of(Amount.of(3L));
+        final Account account = Account.of(History.of(singletonList(
+                OperationStatement.of(DEPOSIT, Amount.of(3L), Amount.of(3L))
+        )));
 
         account.deposit(Amount.of(5L));
 
@@ -51,7 +52,10 @@ public class AccountTest {
 
     @Test
     public void should_have_a_detailed_operation_statement_in_the_account_history_when_i_make_a_withdrawal() {
-        final Account account = Account.of(Amount.of(5L));
+        final Amount balance = Amount.of(5L);
+        final Account account = Account.of(History.of(singletonList(
+                OperationStatement.of(DEPOSIT, balance, balance)
+        )));
 
         account.withdraw(Amount.of(3L));
 
